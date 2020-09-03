180e756f024ad098d6c0f2c2a4dfd57512dd8b61
Gaspard Chevassus <chevassusgaspard@gmail.com>
2020-09-03 09:19:01 +0200

    icons

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /**
  * @format
  */
@@ -11,8 +12,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useNavigation } from '@react-navigation/native';
 
-
-
 const appName = require('./app.json')?.name;
 import Home from './src/pages/home';
 import CNNPage from './src/pages/cnn';
@@ -22,26 +21,35 @@ initSentry();
 
 const ROUTES = {
   HOME: 'OpenPose models',
-  CNN: 'CNN models'
-}
+  CNN: 'CNN models',
+};
 const Stack = createStackNavigator();
 
 const SwitchButton = ({ navigation, route }) => {
   // const [isCnnPage, setIsCnnPage] = useState(true);
   const onPress = () => {
-    console.log(route)
+    console.log(route);
 
     navigation.reset({
       index: 0,
       routes: [{ name: route.name === ROUTES.HOME ? ROUTES.CNN : ROUTES.HOME }],
     });
     // setIsCnnPage(!isCnnPage)
-  }
+  };
 
-  return <Button bg="blue100" h={40} w={40} ml={10} mb={10} rounded="circle" {...{ onPress }}>
-    <Icon fontSize={'xl'} name="swap" color="blue400" />
-  </Button>
-}
+  return (
+    <Button
+      bg="blue100"
+      h={40}
+      w={40}
+      ml={10}
+      mb={10}
+      rounded="circle"
+      {...{ onPress }}>
+      <Icon fontSize={'xl'} name="swap" color="blue400" />
+    </Button>
+  );
+};
 
 export default function Main() {
   const areFontsLoaded = useCachedResources();
@@ -49,7 +57,12 @@ export default function Main() {
   return (
     <ThemeProvider>
       <NavigationContainer>
-        {!areFontsLoaded && <Image resizeMode='contain' style={{ height: '100%', width: '100%' }} source={require("./src/assets/images/splash.png")}></Image>}
+        {!areFontsLoaded && (
+          <Image
+            resizeMode="contain"
+            style={{ height: '100%', width: '100%' }}
+            source={require('./src/assets/images/splash.png')}></Image>
+        )}
         {areFontsLoaded && <NavigationContent />}
       </NavigationContainer>
     </ThemeProvider>
@@ -57,10 +70,18 @@ export default function Main() {
 }
 
 const NavigationContent = () => {
-  const optionsFunc = ({ navigation, route }) => ({ headerLeft: () => SwitchButton({ navigation, route }) })
-  return <Stack.Navigator>
-    <Stack.Screen name={ROUTES.CNN} component={CNNPage} options={optionsFunc} />
-    <Stack.Screen name={ROUTES.HOME} component={Home} options={optionsFunc} />
-  </Stack.Navigator>
-}
+  const optionsFunc = ({ navigation, route }) => ({
+    headerLeft: () => SwitchButton({ navigation, route }),
+  });
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name={ROUTES.CNN}
+        component={CNNPage}
+        options={optionsFunc}
+      />
+      <Stack.Screen name={ROUTES.HOME} component={Home} options={optionsFunc} />
+    </Stack.Navigator>
+  );
+};
 AppRegistry.registerComponent(appName, () => Main);
