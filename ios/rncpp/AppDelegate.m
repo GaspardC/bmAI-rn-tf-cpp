2a63f73a56901921c6e365805ef0284b54b0a688
Gaspard Chevassus <chevassusgaspard@gmail.com>
2020-10-06 18:02:07 +0200

    update rn

--- ios/rncpp/AppDelegate.m
+++ ios/rncpp/AppDelegate.m
@@ -13,7 +13,7 @@
 
 #import <UMCore/UMAppDelegateWrapper.h>
 
-#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
 #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
@@ -52,7 +52,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
 
